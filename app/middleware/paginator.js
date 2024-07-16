bf2343d9ed787d9cd4a5d99f1e1b34198f272f33
Lana Olushola <lolu.sholar@gmail.com>
2024-07-16 08:05:55 +0100

    Bug fixed for truncated data

--- app/middleware/paginator.js
+++ app/middleware/paginator.js
@@ -1,55 +1,58 @@
 const url = require('url');
 
-// Define paginator object
-const pageInfo = {
-	no: null,
-	limit: null,
-	requestUrl: null,
-	requestId: null,
-	active: false
-}
-
 module.exports = () => {
 	return async(req, res, next) => {
+		// Define paginator object
+		const pageInfo = {
+			no: null,
+			limit: null,
+			requestUrl: null,
+			requestId: null,
+			active: false
+		}
+
 		try {
-		  // Get query parameters
-		  [pageInfo.no, pageInfo.limit] = [Number(req.query?.pageNumber), Number(req.query?.pageLimit)]
-		  // Check if active
-		  pageInfo.active = pageInfo.no > 0 && pageInfo.limit > 0
-		  // Get request url
-		  pageInfo.requestUrl = url.parse(req.url, true)
-		  // Get request id
-		  pageInfo.requestId = String(pageInfo.requestUrl.pathname.substr(1)).replace(/[^\w]+/g, '-')
+			// Check that both query variables are set 
+			if (req.query?.pageNumber && req.query?.pageLimit) {
+			  // Get query parameters
+			  [pageInfo.no, pageInfo.limit] = [Number(req.query?.pageNumber), Number(req.query?.pageLimit)]
+			  // Check if active
+			  pageInfo.active = pageInfo.no > 0 && pageInfo.limit > 0
+			  // Get request url
+			  pageInfo.requestUrl = url.parse(req.url, true)
+			  // Get request id
+			  pageInfo.requestId = String(pageInfo.requestUrl.pathname.substr(1)).replace(/[^\w]+/g, '-')
 
-		  // Check if paginator is active
-		  if (pageInfo.active) {
-			  // Grab object send function
-			  const originalSend = res.send
-			  // Hook to new send function
-			  res.send = function (response) {
-			  	// Get response data
-			  	let responseObject = typeof response === 'object' ? response : JSON.parse(response),
-			  			dataObject = responseObject?.data ?? responseObject
+			  // Check if paginator is active
+			  if (pageInfo.active) {
+				  // Grab object send function
+				  const originalSend = res.send
+				  // Hook to new send function
+				  res.send = function (response) {
+				  	// Get response data
+				  	let responseObject = typeof response === 'object' ? response : JSON.parse(response),
+				  			dataObject = responseObject?.data ?? responseObject
 
-			  	// Check if array
-			  	if (Array.isArray(dataObject)) {
-			  		// Define start and end
-			  		const start = (pageInfo.no * pageInfo.limit) - pageInfo.limit,
-			  				end = start + pageInfo.limit
+				  	// Check if array
+				  	if (Array.isArray(dataObject)) {
+				  		// Define start and end
+				  		const start = (pageInfo.no * pageInfo.limit) - pageInfo.limit,
+				  				end = start + pageInfo.limit
 
-			  		// Adjust response result
-			  		const finalForm = {
-			  			...responseObject,
-			  			data: dataObject.slice(start, end),
-			  			pages: Math.ceil(dataObject.length / pageInfo.limit)
-			  		}
+				  		// Adjust response result
+				  		const finalForm = {
+				  			...responseObject,
+				  			data: dataObject.slice(start, end),
+				  			pages: Math.ceil(dataObject.length / pageInfo.limit)
+				  		}
 
-			  		// Stringify response
-			  		response = JSON.stringify(finalForm)
-			  	}
-			  	
-			  	originalSend.call(res, response)
-			  }
+				  		// Stringify response
+				  		response = JSON.stringify(finalForm)
+				  	}
+				  	
+				  	originalSend.call(res, response)
+				  }
+				}
 			}
 		} catch(err) {
 			console.error('Error setting up paginator', err)
